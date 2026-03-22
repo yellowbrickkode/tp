c6585302739e8dd6b828ae9bd8b25594d362ee97
kyrachoo <e1136981@u.nus.edu>
2026-03-22 17:01:31 +0800

    add customer index prefix

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -14,5 +14,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_REGION = new Prefix("r/");
     public static final Prefix PREFIX_ORDERS = new Prefix("o/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_CUSTOMERIDX = new Prefix("c/");
 
 }
